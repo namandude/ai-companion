2c5857d5c0a683e1e28b0019cf0ce25640f056ac
Naman Parashar <namandaksh2005@gmail.com>
2024-06-28 01:28:55 +0530

    chat api

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,78 +1,81 @@
-import { Companion } from '@prisma/client';
-import { auth ,currentUser } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import prismadb from '@/lib/prismadb';
+import prisma from "@/lib/prismadb";
 
-export async function PATCH(req:Request,
-  {params}:{params:{companionId:string}}
-) {
-  try {
-const body = await req.json();
-const user= await currentUser();
-const {src,name,description,instruction,seed,categoryId}=body;
-if(!params.companionId){
-  return new NextResponse ("Companion Id is requires",{status : 400})
-}
-if(!user || !user.id || !user.firstName){
-  return new NextResponse("Unauthorization",{status : 401})
-}
-if(!src || !name || !description ||!instruction || !seed ||!categoryId){
-  return new NextResponse("Missing required fields",{status : 400})
-}
-//TODO CHECK FOR SUBSCRIPTION
+export async function PATCH(req: Request,
+    {params}:{params:{companionId:string}}
+){
+    try{
+        const body= await req.json();
+        const user=await currentUser();
+        const{ src, name, description,instruction,seed,categoryId} =body;
+        if(!user||!user.id||!user.firstName){
+            return new NextResponse("Unauthorized",{status:401});
+        }
+        if(!src||!name||!description||!instruction||!seed||!categoryId){
+            return new NextResponse("Missing required fields",{status:400});
+        }
 
-const companion = await prismadb.companion.update({
-  where:{
-id :params.companionId,
-  },
-  data :{
-    categoryId,
-    userId : user.id,
-    userName:user.firstName,
-    src,
-    name,
-    description,
-    instruction,
-    seed
+        if(!params.companionId){
+            return new NextResponse("Companion ID is required", {status:400});
+        }
 
-  }
-});
-return NextResponse.json(companion);
-  }catch (error){
-    console.log("[COMPANION_PATCH]",error);
-    return new NextResponse("Internal Error",{status:500});
-  }
-  
+        //todo: check subs
+
+        const companion= await prisma.companion.update({
+            where :{
+                id: params.companionId,
+                userId: user.id,
+            },
+            data:{
+               categoryId,
+               userId: user.id,
+               userName:user.firstName,
+               src,
+               name,
+               description,
+               instruction,
+               seed
+            }
+        });
+
+        return NextResponse.json(companion);
+
+    } catch(error){
+        console.log("[COMPANION_PATCH]", error);
+        return new NextResponse("Internal Error")
+    }
 }
+
 
 
 export async function DELETE(req: Request,
-  {params}:{params:{companionId:string}}
+    {params}:{params:{companionId:string}}
 ){
-  try{
-      
-      const {userId}= auth();
+    try{
+        
+        const {userId}= auth();
 
-      
-      if(!userId){
-          return new NextResponse("Unauthorized",{status:401});
-      }
-     
+        
+        if(!userId){
+            return new NextResponse("Unauthorized",{status:401});
+        }
+       
 
-      //todo: check subs
+        //todo: check subs
 
-      const companion= await prismadb.companion.delete({
-          where :{
-              userId,
-              id: params.companionId,
-          }
-      });
+        const companion= await prisma.companion.delete({
+            where :{
+                userId,
+                id: params.companionId,
+            }
+        });
 
-      return NextResponse.json(companion);
+        return NextResponse.json(companion);
 
-  } catch(error){
-      console.log("[COMPANION_DELETE]", error);
-      return new NextResponse("Internal Error", {status:500});
-  }
+    } catch(error){
+        console.log("[COMPANION_DELETE]", error);
+        return new NextResponse("Internal Error", {status:500});
+    }
 }
 
--- app/api/companion/route.ts
+++ app/api/companion/route.ts
@@ -1,38 +1,38 @@
-
 import { currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import prismadb from '@/lib/prismadb';
+import prisma from "@/lib/prismadb";
 
-export async function POST(req:Request) {
-  try {
-const body = await req.json();
-const user= await currentUser();
-const {src,name,description,instruction,seed,categoryId}=body;
-if(!user || !user.id || !user.firstName){
-  return new NextResponse("Unauthorization",{status : 401})
-}
-if(!src || !name || !description ||!instruction || !seed ||!categoryId){
-  return new NextResponse("Missing required fields",{status : 400})
-}
-//TODO CHECK FOR SUBSCRIPTION
+export async function POST(req: Request){
+    try{
+        const body= await req.json();
+        const user=await currentUser();
+        const{ src, name, description,instruction,seed,categoryId} =body;
+        if(!user||!user.id||!user.firstName){
+            return new NextResponse("Unauthorized",{status:401});
+        }
+        if(!src||!name||!description||!instruction||!seed||!categoryId){
+            return new NextResponse("Missing required fields",{status:400});
+        }
 
-const companion = await prismadb.companion.create({
-  data :{
-    categoryId,
-    userId : user.id,
-    userName:user.firstName,
-    src,
-    name,
-    description,
-    instruction,
-    seed
+        //TODO: CHECK FOR SUBSCRIPTION
 
-  }
-});
-return NextResponse.json(companion);
-  }catch (error){
-    console.log("[COMPANION_POST]",error);
-    return new NextResponse("Internal Error",{status:500});
-  }
-  
+        const companion= await prisma.companion.create({
+            data:{
+               categoryId,
+               userId: user.id,
+               userName:user.firstName,
+               src,
+               name,
+               description,
+               instruction,
+               seed
+            }
+        });
+
+        return NextResponse.json(companion);
+
+    } catch(error){
+        console.log("[COMPANION_POST]", error);
+        return new NextResponse("Internal Error")
+    }
 }
